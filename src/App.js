5bc41d12d85da09c6833f84099ffb01156a80186
karen-ifrt <55798470+karen-ifrt@users.noreply.github.com>
2020-03-03 14:48:33 +0100

    score

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ import './App.css'
 import Card from './Card'
 import GuessCount from './GuessCount'
 import HallOfFame, { FAKE_HOF } from './HallOfFame'
+import HighScoreInput from './HighScoreInput'
 
 const SIDE = 6
 const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+const VISUAL_PAUSE_MSECS = 750
 
 class App extends Component {
   state = {
     cards: this.generateCards(),
     currentPair: [],
     guesses: 0,
+    hallOfFame: null,
     matchedCardIndices: [],
+  }
+
+  // Fonction fléchée pour bind
+  displayHallOfFame = hallOfFame => {
+    this.setState({ hallOfFame })
   }
 
   generateCards() {
@@ -33,7 +41,7 @@ class App extends Component {
     const { currentPair, matchedCardIndices } = this.state
     const indexMatched = matchedCardIndices.includes(index)
 
-    if(currentPair.length < 2) {
+    if (currentPair.length < 2) {
       return indexMatched || index === currentPair[0] ? 'visible' : 'hidden'
     }
 
@@ -60,16 +68,33 @@ class App extends Component {
     this.handleNewPairClosedBy(index)
   }
 
+  handleNewPairClosedBy(index) {
+    const { cards, currentPair, guesses, matchedCardIndices } = this.state
+
+    const newPair = [currentPair[0], index]
+    const newGuesses = guesses + 1
+    const matched = cards[newPair[0]] === cards[newPair[1]]
+    this.setState({ currentPair: newPair, guesses: newGuesses })
+
+    if (matched) {
+      this.setState({ matchedCardIndices: [...matchedCardIndices, ...newPair] })
+    }
+
+    setTimeout(() => this.setState({ currentPair: [] }), VISUAL_PAUSE_MSECS)
+  }
+
   render() {
-    const { cards, guesses, matchedCardIndices } = this.state
-    const won = matchedCardIndices.length === cards.length
+    const { cards, guesses, hallOfFame, matchedCardIndices } = this.state
+    const won = matchedCardIndices.length === 4
     return (
       <div className="memory">
         <GuessCount guesses={guesses} />
         {cards.map((card, index) => (
           <Card card={card} feedback={this.getFeedbackForCard(index)} key={index} index={index} onClick={this.handleCardClick} />
         ))}
-        {won && <HallOfFame entries={FAKE_HOF} />}
+        {won && (
+          hallOfFame ? (<HallOfFame entries={hallOfFame} /> ) : ( <HighScoreInput guesses={guesses} onStored={this.displayHallOfFame} /> )
+    )}
       </div>
     )
   }
